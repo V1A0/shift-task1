7649c9340970e3fe21c06bbada44a5ea8f25d7df
v1a0 <54343363+V1A0@users.noreply.github.com>
2020-11-02 22:23:00 +0700

    fix

--- shoppe/main.js
+++ shoppe/main.js
@@ -11,36 +11,36 @@ function getRandHexColor() {
 // Setting style of rand element
 function colorize() {
 
-    let randItemID = getRandInt(0, AllDivs.length-1)
+    let randItemID = getRandInt(0, allDivs.length-1)
     let randColor = getRandHexColor()
 
-    AllDivs[randItemID]
+    allDivs[randItemID]
         .animate(
         {boxShadow: "0 0 20px " + randColor},
         {duration: 1000, easing: "ease-out", fill: "forwards"}
     )
 
-    console.log(`Element colorized: (${randColor}) `, AllDivs[randItemID])
+    console.log(`Element colorized: (${randColor}) `, allDivs[randItemID])
 }
 
 
 // Erasing style of element
 function uncolorize() {
 
-    let randItemID = getRandInt(0, AllDivs.length-1)
+    let randItemID = getRandInt(0, allDivs.length-1)
 
-    AllDivs[randItemID]
+    allDivs[randItemID]
         .animate(
         {boxShadow: "0 0 0 "},
         {duration: 1000, easing: "ease-in", fill: "forwards"}
     )
 
-    console.log('Element uncolorized: ', AllDivs[randItemID])
+    console.log('Element uncolorized: ', allDivs[randItemID])
 }
 
 // main
-let timeStep = 2000 // SET THIS VAR = 2 AND HAVE FUN :^)
-let AllDivs = document.querySelectorAll('div, button, svg')
+const timeStep = 2000// SET THIS VAR = 2 AND HAVE FUN :^)
+let allDivs = document.querySelectorAll('*')
 
 setInterval(colorize, timeStep)
 setInterval(uncolorize, timeStep)
